dde431150942913b62c9c51cfb0bd7e6124aaee5
Abdulaziz-Abduhakhimov <abduhakimovabdulaziz7@gmail.com>
2023-07-09 01:00:15 +0500

    feat: done with project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { getObstacleEvents } from './computer-vision';
 
-
-
 interface AutonomousCar {
     isRunning?: boolean,
 
@@ -10,11 +8,14 @@ interface AutonomousCar {
 
 interface AutonomousCarProps {
     isRunning?: boolean;
+    steeringControl: Steering;
 }
 
 interface Events {
     [mode: string]: boolean
 }
+
+
 
 interface Control {
     execute: (command: string) => void;
@@ -23,7 +24,6 @@ interface Control {
 interface Steering extends Control {
     turn: (direction: string) => void;
 }
-
 
 class SteeringControl implements Steering {
     execute(command: string) {
@@ -35,21 +35,35 @@ class SteeringControl implements Steering {
     }
 }
 
+
+
 class Car implements AutonomousCar {
     isRunning;
+    steeringControl;
 
     constructor(props: AutonomousCarProps) {
         this.isRunning = props.isRunning;
+        this.steeringControl = props.steeringControl; 
     }
 
     respond(events: Events) {
         if (!this.isRunning) {
-            return 'Car is off';
+            return console.log('Car is off');
         }
-        return 'what a hell';
-    }
-    
-}
+        
+        Object.keys(events).forEach(eventKey => {
+            if (!events[eventKey]) return;
 
-const autonomousCar = new Car({isRunning: false});
-console.log(autonomousCar.respond(getObstacleEvents()));
+            if (eventKey == 'ObstacleLeft') {
+                this.steeringControl.turn('right');
+            }
+
+            if (eventKey == 'ObstacleRight') {
+                this.steeringControl.turn('left');
+            }
+        });
+    }
+}
+const steering = new SteeringControl();
+const autonomousCar = new Car({ isRunning: true, steeringControl: steering });
+autonomousCar.respond(getObstacleEvents());
